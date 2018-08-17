86fe3412c064b938a18eda30500e2e97ba081c86
Dhruv Shah <idhruv@Dhruvs-MacBook-Pro.local>
2018-08-17 22:59:33 +0530

    🚀 fired up webpack for less loader

--- src/components/FirstComponent.jsx
+++ src/components/FirstComponent.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './FirstComponent.scss';
+import './FirstComponent.less';
 
 export class FirstComponent extends Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
             }
         ]
     }
